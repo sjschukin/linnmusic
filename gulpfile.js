de35fc567810b58953d52373040c3c5862fb4aca
Sergey Shchukin <s.j.schukin@yandex.ru>
2019-09-23 01:24:32 +0300

    gulp-file update

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,17 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     browserSync = require('browser-sync').create();
-    var reload = browserSync.reload;
 
-var themeRoot = 'web/themes/richtan/';
+var themeRoot = "web/themes/richtan/";
 
 gulp.task('sass', function(done) {
-    gulp.src(themeRoot + 'scss/**/*.scss')
+    gulp.src(themeRoot + "scss/**/*.scss")
         .pipe(sourcemaps.init())
         .pipe(sass())
         .on('error', sass.logError)
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(themeRoot + 'css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+        .pipe(browserSync.reload({stream: true}));
 
     done();
 });
@@ -23,23 +20,23 @@ gulp.task('sass', function(done) {
 gulp.task('browser-sync', function(done) {
     //watch files
     var files = [
-        themeRoot + 'css/*.css',
-        themeRoot + 'js/*.js',
-        themeRoot + 'images/**/*',
-        themeRoot + 'templates/**/*.twig'
+        themeRoot + "css/*.css",
+        themeRoot + "js/*.js",
+        themeRoot + "images/**/*",
+        themeRoot + "templates/**/*.twig"
     ];
     //initialize browsersync
     browserSync.init(files, {
         proxy: "dev.linnmusic.ru"
     });
 
-    browserSync.watch('web/').on('change', reload);
+    //browserSync.watch("web/").on('change', reload);
 
     done();
 });
 
-gulp.task('default', gulp.series('sass', 'browser-sync'), function(done) {
-    gulp.watch(themeRoot + 'scss/**/*.scss', gulp.series('sass'));
-
-    done();
+gulp.task('watch', function() {
+    gulp.watch(themeRoot + "scss/**/*.scss", gulp.series('sass'));
 });
+
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
